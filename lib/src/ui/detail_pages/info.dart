762d4c10e1843d6bd782332e7a97f7dc4a886969
Harry Cheng <chengyuhui1@gmail.com>
2022-10-05 16:34:38 +0800

    Allow launching URL

--- lib/src/ui/detail_pages/info.dart
+++ lib/src/ui/detail_pages/info.dart
@@ -6,8 +6,11 @@ import 'package:flt_vndb/src/utils/hooks.dart';
 import 'package:flt_vndb/src/widgets/language_row.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bbcode/flutter_bbcode.dart';
+import 'package:flutter_bbcode/tags/basic_tags.dart';
+import 'package:flutter_bbcode/tags/tag_parser.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MainPage extends HookWidget {
   final VisualNovel vn;
@@ -118,7 +121,7 @@ class MainPage extends HookWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: vn.aliases!
                   .split("\n")
-                  .map((t) => ListTile(title: Text(t)))
+                  .map((t) => ListTile(title: SelectableText(t)))
                   .toList(),
             ),
           )
@@ -167,6 +170,17 @@ class MainPage extends HookWidget {
           data: vn.description!,
           selectable: true,
           defaultStyle: theme.textTheme.bodySmall,
+          tagsParsers: <AbstractTag>{
+            BoldTag(),
+            StrikeThroughTag(),
+            ItalicTag(),
+            UrlTag(onTap: (url) {
+              launchUrl(Uri.parse(url));
+            }),
+            ColorTag(),
+            UnderlineTag(),
+            QuoteTag(),
+          },
         ),
       );
     }
